c96c2cad752b40c26460fdf3c5fa85c2c0fded57
Mark Tyers <aa7401@coventry.ac.uk>
2020-03-25 11:20:56 +0000

    Simplified the Secure Route

--- routes/secure.js
+++ routes/secure.js
@@ -3,20 +3,14 @@ const Router = require('koa-router')
 
 const router = new Router({ prefix: '/secure' })
 
-/**
- * The secure home page.
- *
- * @name Home Page
- * @route {GET} /
- * @authentication This route requires cookie-based authentication.
- */
 router.get('/', async ctx => {
 	try {
 		console.log(ctx.hbs)
-		if(ctx.hbs.authorised !== true) return ctx.redirect('/login?msg=you need to log in')
+		if(ctx.hbs.authorised !== true) return ctx.redirect('/login?msg=you need to log in&referrer=/secure')
 		await ctx.render('secure', ctx.hbs)
 	} catch(err) {
-		await ctx.render('error', {message: err.message})
+		ctx.hbs.error = err.message
+		await ctx.render('error', ctx.hbs)
 	}
 })
 
